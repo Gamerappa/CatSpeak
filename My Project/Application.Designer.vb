51f71eb1fc14b72c5cc0406b7c8750dde6ae2d87
Big Oof <45898787+BlocklandID47101@users.noreply.github.com>
2019-03-03 22:09:43 -0500

    Summary (required but i dont care)

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.CatSpeak_Concept.Login
+            Me.MainForm = Global.CatSpeak.Login
         End Sub
     End Class
 End Namespace
